2194fdea728bf11f25856108439a6465ad20c1ac
YusufAmat <hoccayusuf@gmail.com>
2022-11-18 14:39:28 +0100

    B2-Hw006-OOP ist erledigt.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,21 @@
+
 public class MainApp {
     public static void main(String[] args) {
+
+        try {
+            Daire daire = new Daire(3.75);
+            System.out.println(daire);
+        } catch (LengthException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        try {
+            Silindir silindir = new Silindir(5.55, 7.25);
+            System.out.println(silindir);
+        } catch (LengthException e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
