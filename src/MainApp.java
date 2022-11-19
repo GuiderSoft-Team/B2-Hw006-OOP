d7716cc22b6309dac3c0faa59daa8f4873e29f9c
Mümin <mozdamar90@hotmail.com>
2022-11-19 11:23:20 +0300

    Mümin : B2-Hw006-OOP has done.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,21 @@
 public class MainApp {
     public static void main(String[] args) {
 
+        Daire daire = new Daire(3.75);
+        System.out.println("Daire...");
+        System.out.print("Yarıçap="+daire.getYaricap()+"\t");
+        System.out.printf("Alan  =%5.2f\n ",daire.getAlan());
+        System.out.println();
+        System.out.println("Silindir...");
+        Silindir silindir = new Silindir(5.55, 7.25);
+        System.out.printf("Yarıçap  =%5.2f\n", silindir.getYaricap());
+        System.out.printf("Yükseklik=%5.2f\n", silindir.getYukseklik());
+        System.out.printf("Alan     =%5.2f\n", silindir.getAlan());
+        System.out.printf("Hacim    =%5.2f", silindir.getHacim());
+
     }
 }
+
+
+
+
