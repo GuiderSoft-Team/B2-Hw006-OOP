91c1d2adeb8fbd561fc6414797496530c67135fb
suleymandurmaz <sdurmaz77@hotmail.com>
2022-11-19 10:53:21 +0300

    Suleyman has done

---
+++ src/Daire.java
@@ -0,0 +1,28 @@
+public class Daire {
+    private  double yaricap;
+
+    public Daire(double yaricap) {
+        setYaricap(yaricap);
+    }
+
+    public double getYaricap() {
+        return yaricap;
+    }
+
+    public void setYaricap(double yaricap) {
+        if (yaricap > 0) {
+            this.yaricap = yaricap;
+        } else {
+            this.yaricap = 0;
+        }
+
+    }
+
+  public double getAlan(){
+
+
+      return yaricap * yaricap * Math.PI;
+  }
+
+}
+
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,16 @@
 public class MainApp {
     public static void main(String[] args) {
+        Daire daire = new Daire(3.75);
+        System.out.println("Daire...");
+        System.out.print("Yarıçap= "+daire.getYaricap()+"\t");
+        System.out.printf("Alan= %5.2f ",daire.getAlan());
+        System.out.println();
+        System.out.println("Silindir...");
+        Silindir silindir = new Silindir(5.55, 7.25);
+        System.out.printf("Yarıçap= %5.2f", silindir.getYaricap());
+        System.out.printf("Yükseklik= %5.2f", silindir.getYukseklik());
+        System.out.printf("Alan=%5.2f ",silindir.getAlan());
+        System.out.printf("Hacim= %5.2f " , silindir.getHacim());
 
     }
 }
