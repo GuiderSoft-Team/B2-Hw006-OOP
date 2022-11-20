842aae390b4823234541ec53d2cfbc08ae50090c
Çağla <mariliith@hotmail.com>
2022-11-20 22:22:28 +0300

    Çağlasu Büke Homework is done. 20/11/2022

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,16 @@
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println("Daire...");
+        Daire daire=new Daire(3.75);
+        System.out.printf("Yaricap= %5.2f\n",daire.getYaricap());
+        System.out.printf("Alan= %5.2f\n",daire.getAlan());
+        System.out.println("");
+        System.out.println("Silindir...");
+        Silindir silindir=new Silindir(5.55, 7.25);
+        System.out.printf("Yaricap= %5.2f\n",silindir.getYaricap());
+        System.out.printf("Yukseklik= %5.2f\n",silindir.getYukseklik());
+        System.out.printf("Alan= %5.2f\n",silindir.getAlan());
+        System.out.printf("Hacim= %5.2f",silindir.getHacim());
 
     }
 }
