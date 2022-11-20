d0ef38b5187bb45dff9c46695756c28a4e33016a
yasinmurat <muratyeni671@gmail.com>
2022-11-20 13:39:40 +0300

    Yasin homework5 has done

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,17 @@
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println("--DAİRE--");
+        Daire daire=new Daire(3.75);
+        System.out.println("Yarıçap ="+daire.getYariCap());
+        System.out.println("Alan ="+daire.getAlan());
+        System.out.println("**************");
+        System.out.println("--SİLİNDİR--");
+        Silindir silindir=new Silindir(5.55,7.25);
+        System.out.println("Yarıçap ="+silindir.getYariCap());
+        System.out.println("Yükseklik ="+silindir.getYukseklik());
+        System.out.println("Alan = " +silindir.getAlan());
+        System.out.println("Hacim ="+silindir.getHacim());
+
 
     }
 }
