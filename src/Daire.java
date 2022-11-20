97b252d3c327df053af338c5da7d5e74feeb33d2
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-20 23:55:32 +0300

    Burcu Uğur- Homework6 is done

---
+++ src/Daire.java
@@ -0,0 +1,28 @@
+public class Daire {
+
+    public double yaricap;
+
+    public Daire() {
+    }
+
+    public Daire(double yaricap) {
+        if (yaricap < 0) {
+        this.yaricap=0;
+        }else {
+        this.yaricap=yaricap;
+        }
+    }
+
+    public double getYaricap() {
+        return yaricap;
+    }
+
+    public double getAlan() {
+        return yaricap*yaricap*Math.PI;
+    }
+
+
+
+    }
+
+
