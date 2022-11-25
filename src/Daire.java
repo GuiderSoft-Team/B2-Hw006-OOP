8e339e990c890b32db80ef0102e781ec643d4edb
Gürhan Ustali <gurhanustali@gmail.com>
2022-11-25 21:27:39 +0100

    Has been solved.

---
+++ src/Daire.java
@@ -0,0 +1,20 @@
+public class Daire {
+    private double yaricap;
+
+    public Daire(double yaricap) {
+        setYaricap(yaricap);
+    }
+
+    public double getYaricap() {
+        return yaricap;
+    }
+
+    public void setYaricap(double yaricap) {
+        if(yaricap<0) yaricap=0;
+        this.yaricap = yaricap;
+    }
+
+    public double getAlan(){
+        return Math.PI*Math.pow(yaricap,2);
+    }
+}
---
+++ src/Silindir.java
@@ -0,0 +1,26 @@
+public class Silindir extends Daire{
+    private double yukseklik;
+    public Silindir(double yaricap, double yukseklik) {
+        super(yaricap);
+        setYukseklik(yukseklik);
+    }
+
+    public double getYukseklik() {
+        return yukseklik;
+    }
+
+    public void setYukseklik(double yukseklik) {
+        if(yukseklik<0) yukseklik=0;
+        this.yukseklik = yukseklik;
+    }
+
+
+    public double getHacim(){
+        return super.getAlan()*yukseklik;
+    }
+
+    @Override
+    public double getAlan() {
+        return 2*super.getAlan()+2*Math.PI*getYaricap()*yukseklik;
+    }
+}
